db87c2008a7dafc74b112b9f3c061b6a186963ed
Rachel Maia <89589862+ArquitetaRachel@users.noreply.github.com>
2026-03-02 22:52:11 -0300

    Update count.js
    Atualização do código da contagem

--- js/count.js
+++ js/count.js
@@ -1,16 +1,17 @@
 function countobj(){
-	this.text;
-	this.create = function(){
-		var style = { font: "bold 28px Arial", fill: "#ffffff",boundsAlignH: "left"};
-		var x = 0;
-		var y =30;
-		var str = 'Pontos '+score;
-		this.text = game.add.text(x,y,str,style);
-		this.text.setTextBounds(30, 0, 800, 600);
-	}
-	this.update = function(){
-		var str = 'Pontos '+score+;
-		this.text.setText(str);
-	}
-	this.create();
+
+    this.text;
+
+    this.create = function(){
+        this.text = game.add.text(20,20,"Score: 0",{
+            font:"24px Arial",
+            fill:"#ffffff"
+        });
+    }
+
+    this.update = function(){
+        this.text.text = "Score: " + score;
+    }
+
+    this.create();
 }
