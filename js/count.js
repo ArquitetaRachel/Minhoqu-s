8a9e986c98cffa463e255d508653a6814a203f71
Rachel Maia <89589862+ArquitetaRachel@users.noreply.github.com>
2026-03-02 22:44:26 -0300

    Update count.js
    Atualização da string

--- js/count.js
+++ js/count.js
@@ -4,12 +4,12 @@ function countobj(){
 		var style = { font: "bold 28px Arial", fill: "#ffffff",boundsAlignH: "left"};
 		var x = 0;
 		var y =30;
-		var str = 'Pontos'+score;
+		var str = 'Pontos '+score;
 		this.text = game.add.text(x,y,str,style);
 		this.text.setTextBounds(30, 0, 800, 600);
 	}
 	this.update = function(){
-		var str = '打中'+score+'个怪物';
+		var str = 'Pontos '+score+;
 		this.text.setText(str);
 	}
 	this.create();
